5bfd6f1c0ec33c459458eebbaea9afe1bf0f1fa9
Alexandre <alexandre.milani00@gmail.com>
2022-04-20 19:07:53 -0400

    pc_pb_2021_cargo_7.tecnologia

--- pc_pb_2021_cargo_7.py
+++ pc_pb_2021_cargo_7.py
@@ -2,7 +2,7 @@ tecnologia = [
     {
         'q': [
             '<!-- PC-PB 2021 - Cargo 7 Questão 52 -->',
-            '''O esquema RAID que possui dois cálculos de paridade diferentes, executados e armazenados em blocos separados em discos distintos, e que necessita de D+2 discos, em que D é a quantidade de discos necessária para armazenar os dados do usuário, é o''',
+            'O esquema RAID que possui dois cálculos de paridade diferentes, executados e armazenados em blocos separados em discos distintos, e que necessita de D+2 discos, em que D é a quantidade de discos necessária para armazenar os dados do usuário, é o',
         ],
         'e': [
             'RAID 6.',
@@ -17,7 +17,7 @@ tecnologia = [
         'q': [
             '<!-- PC-PB 2021 - Cargo 7 Questão 54 -->',
             '![](./pc_pb_2021_cargo_7/pc_pb_2021_cargo_7_54.png)',
-            '''O tipo de virtualização antes ilustrado, em que a máquina virtual enxerga uma abstração do hardware não idêntica à do hardware físico, que requer modificação do sistema operacional convidado, é conhecido como''',
+            'O tipo de virtualização antes ilustrado, em que a máquina virtual enxerga uma abstração do hardware não idêntica à do hardware físico, que requer modificação do sistema operacional convidado, é conhecido como',
         ],
         'e': [
             'virtualização embasada em translação binária.',
@@ -31,7 +31,7 @@ tecnologia = [
     {
         'q': [
             '<!-- PC-PB 2021 - Cargo 7 Questão 55 -->',
-            '''O serviço no qual o provedor de nuvem fornece servidores, armazenamento, rede e ferramentas para desenvolver, testar, hospedar e entregar aplicativos e os clientes podem usar um conjunto de ferramentas pré-montadas é conhecido como''',
+            'O serviço no qual o provedor de nuvem fornece servidores, armazenamento, rede e ferramentas para desenvolver, testar, hospedar e entregar aplicativos e os clientes podem usar um conjunto de ferramentas pré-montadas é conhecido como',
         ],
         'e': [
             'Containers as a Service (CaaS).',
@@ -45,7 +45,7 @@ tecnologia = [
     {
         'q': [
             '<!-- PC-PB 2021 - Cargo 7 Questão 56 -->',
-            '''No sistema de arquivos NTFS, presente no Windows Server 2012 R2, o arquivo de sistema que possui pelo menos uma entrada para cada arquivo de um volume NTFS, inclusive a do próprio arquivo de sistema, e que guarda todas as informações sobre um arquivo, incluindo tamanho e permissões, é o''',
+            'No sistema de arquivos NTFS, presente no Windows Server 2012 R2, o arquivo de sistema que possui pelo menos uma entrada para cada arquivo de um volume NTFS, inclusive a do próprio arquivo de sistema, e que guarda todas as informações sobre um arquivo, incluindo tamanho e permissões, é o',
         ],
         'e': [
             'volume boot record (VBR).',
@@ -56,10 +56,30 @@ tecnologia = [
         ],
         'r': '',
     },
+    {
+        'q': [
+            '<!-- PC-PB 2021 - Cargo 7 Questão 57 -->',
+            'Python é uma linguagem procedural que utiliza quatro tipos de dados predefinidos para lidar com coleções: conjuntos, dicionários, listas e tuplas. A respeito desses tipos de dados, julgue os itens a seguir.',
+            '\n'.join([
+                'i. O conjunto permite o armazenamento de uma tupla, mas não o de uma lista.',
+                'ii. A tupla é idêntica à lista, exceto pela forma mais simples com que sua declaração é realizada.',
+                'iii. A lista é um tipo de dados variável que permite a alteração de seus elementos após a sua criação.',
+            ]),
+            'Assinale a opção correta.',
+        ],
+        'e': [
+            'Apenas o item I está certo.',
+            'Todos os itens estão certos.',
+            'Apenas o item II está certo.',
+            'Apenas os itens I e III estão certos.',
+            'Apenas os itens II e III estão certos.',
+        ],
+        'r': '',
+    },
     #{
         #'q': [
             #'<!-- PC-PB 2021 - Cargo 7 Questão xx -->',
-            #'''''',
+            #'',
         #],
         #'e': [
         #],
--- prova.md
+++ prova.md
@@ -3,47 +3,39 @@
 
 Questão 1
 
-<!-- PC-PB 2021 - Cargo 7 Questão 56 -->
-
-No sistema de arquivos NTFS, presente no Windows Server 2012 R2, o arquivo de sistema que possui pelo menos uma entrada para cada arquivo de um volume NTFS, inclusive a do próprio arquivo de sistema, e que guarda todas as informações sobre um arquivo, incluindo tamanho e permissões, é o
-
-- a. volume boot record (VBR).
-- b. secure - security file.
-- c. extend - NTFS extension file.
-- d. bitmap - cluster bitmap.
-- e. master file table (MFT).
-
----
-
-Questão 2
-
 <!-- PC-PB 2021 - Cargo 7 Questão 55 -->
 
 O serviço no qual o provedor de nuvem fornece servidores, armazenamento, rede e ferramentas para desenvolver, testar, hospedar e entregar aplicativos e os clientes podem usar um conjunto de ferramentas pré-montadas é conhecido como
 
-- a. Platform as a Service (PaaS).
-- b. Containers as a Service (CaaS).
+- a. Containers as a Service (CaaS).
+- b. Platform as a Service (PaaS).
 - c. Infrastructure as a Service (IaaS).
 - d. Function as a Service (FaaS).
 - e. Service as a Service (SaaS).
 
 ---
 
-Questão 3
+Questão 2
 
-<!-- PC-PB 2021 - Cargo 7 Questão 52 -->
+<!-- PC-PB 2021 - Cargo 7 Questão 57 -->
 
-O esquema RAID que possui dois cálculos de paridade diferentes, executados e armazenados em blocos separados em discos distintos, e que necessita de D+2 discos, em que D é a quantidade de discos necessária para armazenar os dados do usuário, é o
+Python é uma linguagem procedural que utiliza quatro tipos de dados predefinidos para lidar com coleções: conjuntos, dicionários, listas e tuplas. A respeito desses tipos de dados, julgue os itens a seguir.
 
-- a. RAID 1.
-- b. RAID 6.
-- c. RAID 0.
-- d. RAID 5.
-- e. RAID 3.
+i. O conjunto permite o armazenamento de uma tupla, mas não o de uma lista.
+ii. A tupla é idêntica à lista, exceto pela forma mais simples com que sua declaração é realizada.
+iii. A lista é um tipo de dados variável que permite a alteração de seus elementos após a sua criação.
+
+Assinale a opção correta.
+
+- a. Apenas os itens I e III estão certos.
+- b. Todos os itens estão certos.
+- c. Apenas o item I está certo.
+- d. Apenas os itens II e III estão certos.
+- e. Apenas o item II está certo.
 
 ---
 
-Questão 4
+Questão 3
 
 <!-- PC-PB 2021 - Cargo 7 Questão 54 -->
 
@@ -51,15 +43,43 @@ Questão 4
 
 O tipo de virtualização antes ilustrado, em que a máquina virtual enxerga uma abstração do hardware não idêntica à do hardware físico, que requer modificação do sistema operacional convidado, é conhecido como
 
-- a. virtualização por hypervisor.
-- b. paravirtualização.
+- a. paravirtualização.
+- b. virtualização total.
 - c. virtualização embasada em translação binária.
-- d. virtualização total.
+- d. virtualização por hypervisor.
 - e. virtualização assistida.
+
+---
+
+Questão 4
+
+<!-- PC-PB 2021 - Cargo 7 Questão 56 -->
+
+No sistema de arquivos NTFS, presente no Windows Server 2012 R2, o arquivo de sistema que possui pelo menos uma entrada para cada arquivo de um volume NTFS, inclusive a do próprio arquivo de sistema, e que guarda todas as informações sobre um arquivo, incluindo tamanho e permissões, é o
+
+- a. extend - NTFS extension file.
+- b. bitmap - cluster bitmap.
+- c. volume boot record (VBR).
+- d. secure - security file.
+- e. master file table (MFT).
+
+---
+
+Questão 5
+
+<!-- PC-PB 2021 - Cargo 7 Questão 52 -->
+
+O esquema RAID que possui dois cálculos de paridade diferentes, executados e armazenados em blocos separados em discos distintos, e que necessita de D+2 discos, em que D é a quantidade de discos necessária para armazenar os dados do usuário, é o
+
+- a. RAID 5.
+- b. RAID 0.
+- c. RAID 6.
+- d. RAID 3.
+- e. RAID 1.
 
 ---
 
 |       | Gabarito |       |
 | :---: | :------: | :---: |
 | 1 - Nulo | 2 - Nulo | 3 - Nulo |
-| 4 - Nulo |  |  |
+| 4 - Nulo | 5 - Nulo |  |
